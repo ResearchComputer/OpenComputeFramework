eb253965023d8ee583e4adda99f92eb2822b4a45
xzyao <askxzyao@gmail.com>
2023-06-25 15:36:27 +0000

    update

--- examples/apis/inference_example.py
+++ examples/apis/inference_example.py
@@ -1,21 +1,10 @@
 import requests
-from constant import RELAY_URL, HOST_ID
 from multiprocessing import Pool
 
-def inference(i):
-    url = f"{RELAY_URL}/api/v1/proxy/{HOST_ID}/api/v1/request/inference"
-    resp = requests.post(url, json={
-        'model_name': 'openlm-research/open_llama_7b',
-        'params': {
-            'prompt': "Hello!"
-        }
-    })
-    print(resp.text)
-    return resp.text
+URL = "https://inference.autoai.dev/api/v1/request/inference"
 
 def global_inference(i):
-    url = f"{RELAY_URL}/api/v1/request/inference"
-    resp = requests.post(url, json={
+    resp = requests.post(URL, json={
         'model_name': 'togethercomputer/RedPajama-INCITE-7B-Base',
         'params': {
             'prompt': "Alan Turing was a "
