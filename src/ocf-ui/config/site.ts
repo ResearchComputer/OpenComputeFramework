5349cdb6af756f3fa9d8d879d059afb2925fa714
xzyao <askxzyao@gmail.com>
2023-06-26 14:46:33 +0000

    initialize ui

---
+++ src/ocf-ui/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "ocf/ui",
+  url: "https://ocf.autoai.org",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description:
+    "Open, Composable and Flexible Computing Infrastructure for AI",
+  links: {
+    twitter: "https://twitter.com/autoaiHQ",
+    github: "https://github.com/autoai-org/ocf",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
