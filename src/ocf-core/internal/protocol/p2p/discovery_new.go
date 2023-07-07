f8fc3f29658bfa0b9bebefff7d7fbacca5c6ff30
xzyao <askxzyao@gmail.com>
2023-07-07 12:23:57 +0000

    wip: protocol rewritten

--- src/ocf-core/internal/protocol/p2p/discovery_new.go
+++ src/ocf-core/internal/protocol/p2p/discovery_new.go
@@ -2,16 +2,20 @@ package p2p
 
 import (
 	"context"
+	"ocfcore/internal/common"
 	"sync"
 	"time"
 
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peerstore"
 	routing "github.com/libp2p/go-libp2p/p2p/discovery/routing"
 )
 
 var discoverLockNew sync.Mutex
 
+// DiscoverNew is a function that keeps updating DNT with the latest information about the network.
 func DiscoverNew(ctx context.Context, h host.Host, dht *dht.IpfsDHT, rendezvous string) {
 	discoverLockNew.Lock()
 	defer discoverLockNew.Unlock()
@@ -19,5 +23,32 @@ func DiscoverNew(ctx context.Context, h host.Host, dht *dht.IpfsDHT, rendezvous 
 	routingDiscovery.Advertise(ctx, rendezvous)
 	ticker := time.NewTicker(time.Second * 1)
 	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			peers, err := routingDiscovery.FindPeers(ctx, rendezvous)
+			if err != nil {
+				common.Logger.Error(err)
+			}
+			for p := range peers {
+				if p.ID == h.ID() {
+					continue
+				}
+				if h.Network().Connectedness(p.ID) != network.Connected {
+					_, err = h.Network().DialPeer(ctx, p.ID)
+					if err != nil {
+						continue
+					}
+				}
+				common.Logger.Debug("Connectivity to peer: ", p.ID, " [", h.Network().Connectedness(p.ID), "] ")
+				if h.Network().Connectedness(p.ID) == network.Connected {
+					h.Peerstore().AddAddrs(p.ID, p.Addrs, peerstore.PermanentAddrTTL)
+					// add to tempDNT
 
+				}
+			}
+		}
+	}
 }
--- src/ocf-core/internal/protocol/p2p/structs.go
+++ src/ocf-core/internal/protocol/p2p/structs.go
@@ -14,17 +14,55 @@ type HardwareSpec struct {
 	UsedMemory      int64     `json:"host_memory_used"`
 }
 
-// OCFPeer is a single node in the network, as can be seen by the current node.
-type OCFPeer struct {
-	PeerID          string
-	Latency         int // in
-	Privileged      bool
-	Owner           string
-	CurrentOffering []string
-	Hardware        []HardwareSpec
-	Role            []string
+type PeerStatus string
+
+const (
+	CONNECTED    PeerStatus = "connected"
+	DISCONNECTED PeerStatus = "disconnected"
+)
+
+// Peer is a single node in the network, as can be seen by the current node.
+type Peer struct {
+	PeerID          string         `json:"peer_id"`
+	Latency         int            `json:"latency"` // in ms
+	Privileged      bool           `json:"privileged"`
+	Owner           string         `json:"owner"`
+	CurrentOffering []string       `json:"current_offering"`
+	Hardware        []HardwareSpec `json:"hardware"`
+	Role            []string       `json:"role"`
+	Status          string         `json:"status"`
+	Service         string         `json:"service"`
 }
 
 // Node table tracks the nodes and their status in the network.
 type NodeTable struct {
+	Peers []Peer `json:"peers"`
+}
+
+func (dnt NodeTable) Update(peer Peer) *NodeTable {
+	for idx, n := range dnt.Peers {
+		if n.PeerID == peer.PeerID {
+			if peer.Status == "disconnected" {
+				dnt.Peers = append(dnt.Peers[:idx], dnt.Peers[idx+1:]...)
+				return &dnt
+			} else if peer.Status == "connected" {
+				dnt.Peers[idx] = peer
+			}
+			return &dnt
+		}
+	}
+	if peer.Status == "connected" {
+		dnt.Peers = append(dnt.Peers, peer)
+	}
+	return &dnt
+}
+
+func (dnt NodeTable) FindProviders(service string) []Peer {
+	var providers []Peer
+	for _, n := range dnt.Peers {
+		if n.Service == service {
+			providers = append(providers, n)
+		}
+	}
+	return providers
 }
