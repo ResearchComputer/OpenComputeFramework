4fcb2159ac63940fcd230b803d05a434ce7ed127
xzyao <askxzyao@gmail.com>
2024-07-31 15:07:05 +0000

    disable tracker when env var is not set

--- src/internal/server/tracer.go
+++ src/internal/server/tracer.go
@@ -14,37 +14,44 @@ import (
 var (
 	dataset    = os.Getenv("AXIOM_DATASET")
 	tracerOnce sync.Once
-	tracker    *axiom.Client
+	tracker    *axiom.Client = nil
 )
 
 func initTracer() {
 	tracerOnce.Do(func() {
-		ctx := context.Background()
-		stop, err := axiotel.InitTracing(ctx, dataset, "research-computer-coordinator", "v1.0.0")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer func() {
-			if stopErr := stop(); stopErr != nil {
-				log.Fatal(stopErr)
+		if dataset == "" {
+			tracker = nil
+			common.Logger.Info("AXIOM_DATASET not set, tracing disabled")
+		} else {
+			ctx := context.Background()
+			stop, err := axiotel.InitTracing(ctx, dataset, "research-computer-coordinator", "v1.0.0")
+			if err != nil {
+				log.Fatal(err)
 			}
-		}()
-		tracker, err = axiom.NewClient()
-		if err != nil {
-			log.Fatal(err)
+			defer func() {
+				if stopErr := stop(); stopErr != nil {
+					log.Fatal(stopErr)
+				}
+			}()
+			tracker, err = axiom.NewClient()
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	})
 }
 
 func IngestEvents(events []axiom.Event) {
-	go func() {
-		// expand events to axiom.Event
-		res, err := tracker.IngestEvents(context.Background(), dataset, events)
-		if err != nil {
-			common.Logger.Error(err)
-		}
-		for _, fail := range res.Failures {
-			common.Logger.Error(fail)
-		}
-	}()
+	if tracker != nil {
+		go func() {
+			// expand events to axiom.Event
+			res, err := tracker.IngestEvents(context.Background(), dataset, events)
+			if err != nil {
+				common.Logger.Error(err)
+			}
+			for _, fail := range res.Failures {
+				common.Logger.Error(fail)
+			}
+		}()
+	}
 }
