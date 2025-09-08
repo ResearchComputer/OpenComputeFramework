59551c7ab43c99272b6711ff41e29526fe779d96
Xiaozhe Yao <askxzyao@gmail.com>
2025-09-09 01:34:24 +0200

    update ci

--- meta/build_core_docker.sh
+++ meta/build_core_docker.sh
@@ -1,1 +1,17 @@
-docker build -f meta/Dockerfile.amd64 -t ghcr.io/researchcomputer/ocf:amd64-latest . && docker push ghcr.io/researchcomputer/ocf:amd64-latest
+#!/bin/bash
+
+# Default to latest if no version is specified
+VERSION=${1:-latest}
+
+# Validate version
+case $VERSION in
+    stable|latest|dev)
+        ;;
+    *)
+        echo "Error: Version must be 'stable', 'latest', or 'dev'"
+        exit 1
+        ;;
+esac
+
+# Build and push with the specified version
+docker build -f meta/Dockerfile.amd64 -t ghcr.io/researchcomputer/ocf:amd64-$VERSION . && docker push ghcr.io/researchcomputer/ocf:amd64-$VERSION
